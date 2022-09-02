d80907feaf81a4ce38ce84591ec45789438c0c8f
Nekkantipravalika <107562680+Nekkantipravalika@users.noreply.github.com>
2022-09-02 19:25:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,13 @@ def count_batteries_by_usage(cycles):
     }
     
     for cyc in cycles:
-        
         if cyc < 400:
             dict['lowCount'] += 1
-            
         elif cyc < 919:
             dict['mediumCount'] += 1
-            
         else:
             dict['highCount'] += 1
-            
-    return dic
+   return dic
    
  
   
